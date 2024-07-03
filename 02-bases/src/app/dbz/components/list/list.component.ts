a3b6784535728754a143157cdcbca59cb584aecd
Fernando Garcia <gfernandoentique97@gmail.com>
2024-07-03 10:20:46 -0600

    fin seccion 5

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -14,4 +14,14 @@ export class ListComponent {
       power: 10,
     },
   ];
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void {
+    //TODO Emitir el Id del personaje
+    if (!id) return;
+    // console.log({ id });
+    this.onDelete.emit(id);
+  }
 }
