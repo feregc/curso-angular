fd0508bc10051d277c574138fff14a8c087f7fa1
Fernando Garcia <gfernandoentique97@gmail.com>
2024-07-03 15:51:18 -0600

    seccion 7 pt1

---
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  private _tagHistory: string[] = [];
+
+  constructor() {}
+
+  get tagsHistory() {
+    return [...this._tagHistory];
+  }
+
+  private orginazeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
+    }
+
+    this._tagHistory.unshift(tag);
+    this._tagHistory = this._tagHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.orginazeHistory(tag);
+  }
+}
